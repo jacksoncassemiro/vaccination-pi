a3c70aaa7bea7edf85f1831586bab34a27a1338c
Jackson Cassemiro <jacksondgls@live.com>
2025-04-21 00:25:57 -0300

    feat: adicionado e configurado o tailwind css v4
    Signed-off-by: Jackson Cassemiro <jacksondgls@live.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
+import "..styles/globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+	? `https://${process.env.VERCEL_URL}`
+	: "http://localhost:3000";
 
 export const metadata: Metadata = {
+	metadataBase: new URL(defaultUrl),
 	title: "Todolist PI",
 	description:
 		"Projeto desenvolvido para o curso de Sistemas para Internet da UNCISAL.",
